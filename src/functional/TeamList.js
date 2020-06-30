45e764e1420d935ec01f241c1592db34bfb8dd4f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-06-30 12:33:39 -0400

    add player form to map

--- src/functional/TeamList.js
+++ src/functional/TeamList.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import axios from 'axios'
+import config from '../config'
 
 import { connect } from 'react-redux'
 
-const add_slot = () => {
-    console.log('clicked')
-    return (
-        <div>
-            <input type="text" placeholder="player name" />
-        </div>
-    )
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+
+    axios.patch(`${config.API_ENDPOINT}/upate/`)
 }
 
 
@@ -22,18 +23,18 @@ const TeamList = (props) => (
         <h2>{props.country.team_name}</h2>
         {props.countries.length > 0 ?
           props.countries.map(tm => 
-          (<div>
-          <h3 key={tm.id} >{tm.player_name}</h3>
-            <h3 key={tm.id}>{tm.player_name}</h3>
+          (<div key={tm.id}>
+                <h3>{tm.player_name}</h3>
+                <h3>{tm.player_name}</h3>
+            <form onSubmit={() => handleSubmit()}>
+                <input type="text" placeholder="player name" />
+                <button type="submit" >add player</button>
+            </form>
            </div>
             )
         ) : null
 
         }
-        <form>
-        <input type="text" placeholder="player name" />
-        <button onClick={() => add_slot()}>add player</button>
-        </form>
     </div>
         
     </div>
